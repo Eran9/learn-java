2d6ec1815c2069b91c55c546345a6368e9901ed0
Eran9 <eran.perera2010@gmal.com>
2024-06-03 23:37:25 +0530

    Minor bugfixes

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -5,42 +5,74 @@ public class Main {
     int userSelection;
 
     public int Patient(){
-        System.out.println("""
+        while(true){
+            System.out.println("""
                             
                             Enter 1 to view doctors
                             Enter 2 to book an appointment
                             Enter 3 to view a selected doctor's bookings
                             Enter 4 to register patient
                             Enter 5 to exit""");
-        System.out.print("Enter input: ");
-        userSelection = scanner.nextInt();
-        return userSelection;
+            System.out.print("Enter input: ");
+            userSelection = scanner.nextInt();
+            if(userSelection <= 0 || userSelection > 5){
+                System.out.println("Invalid Input! Please check and try again...");
+            } else if (userSelection == 5) {
+                System.out.println("Exiting... Have a nice day.");
+                System.exit(0);
+            } else {
+                return userSelection;
+            }
+        }
     }
 
     public int HospitalAdministrator(){
-        System.out.println("""
+        while(true){
+            System.out.println("""
                             
                             Enter 1 to add a doctor
                             Enter 2 to add a doctor availability
                             Enter 3 to exit""");
-        System.out.print("Enter input: ");
-        userSelection = scanner.nextInt();
-        return userSelection;
+            System.out.print("Enter input: ");
+            userSelection = scanner.nextInt();
+            if (userSelection <= 0 || userSelection > 3){
+                System.out.println("Invalid Input! Please check and try again...");
+            } else if (userSelection == 3) {
+                System.out.println("Exiting... Have a nice day");
+                System.exit(0);
+            } else{
+                return userSelection;
+            }
+        }
     }
 
     public int MainMenu(){
-        System.out.println("""
+        while(true) {
+            System.out.println("""
                     
                     If you are a hospital administrator please enter 1.
                     If you are a patient please enter 2.
                     If you want to exit please enter 3.""");
-        System.out.print("Enter input: ");
-        int authorityLevel = scanner.nextInt(10);
-        return authorityLevel;
+            System.out.print("Enter input: ");
+            int authorityLevel = scanner.nextInt(10);
+            if (authorityLevel <= 0 || authorityLevel > 3) {
+                System.out.println("Invalid Input! Please check and try again...");
+            } else if (authorityLevel == 3) {
+                System.out.println("Exiting... Have a nice day");
+                System.exit(0);
+            } else {
+                return authorityLevel;
+            }
+        }
     }
 
     public static void main(String[] args) {
-
-
+        Main m = new Main();
+        int authortiy = m.MainMenu();
+        if (authortiy == 1){
+            m.HospitalAdministrator();
+        } else if (authortiy == 2) {
+            m.Patient();
+        }
     }
 }
